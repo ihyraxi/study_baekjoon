c431d14e6553472373b0222c9fa1e5e82672b0c0
ihyraxi <100103351+ihyraxi@users.noreply.github.com>
2022-02-26 23:17:37 +0900

    BOJ-10699 '오늘 날짜' [ 22/02/26 23:17:37 ] by Alub

---
+++ BOJ/10699/10699_1.py
@@ -0,0 +1,10 @@
+'''
+ 메모리: 35888 KB, 시간: 96 ms
+ 2022.02.26
+ by Alub
+'''
+import datetime
+print(str(datetime.datetime.now())[:10])
+#datetime.datetime.now()는 현재의 시간을 출력해줌.
+#datetime 모듈을 이용해서 현재시간을 가져옴.
+#[:10]을 통해 앞의 년-월-일 정보, 즉, 10개의 문자를 출력함.
