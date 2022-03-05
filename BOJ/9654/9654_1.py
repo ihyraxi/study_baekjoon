671cdd281ded53f08858a8ac3ef7227580ad90e8
ihyraxi <100103351+ihyraxi@users.noreply.github.com>
2022-03-05 23:57:25 +0900

    BOJ-9654 '나부 함대 데이터' [ 22/03/05 23:57:25 ] by Alub

---
+++ BOJ/9654/9654_1.py
@@ -0,0 +1,11 @@
+'''
+ 메모리: 30864 KB, 시간: 72 ms
+ 2022.03.05
+ by Alub
+'''
+print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE")
+print("N2 Bomber      Heavy Fighter  Limited    21        ")
+print("J-Type 327     Light Combat   Unlimited  1         ")
+print("NX Cruiser     Medium Fighter Limited    18        ")
+print("N1 Starfighter Medium Fighter Unlimited  25        ")
+print("Royal Cruiser  Light Combat   Limited    4         ")
