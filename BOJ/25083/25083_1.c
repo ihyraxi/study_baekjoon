f5e8be16642c85ad14b8b38be1fd1456da4a83c6
ihyraxi <100103351+ihyraxi@users.noreply.github.com>
2022-05-17 09:26:15 +0900

    BOJ-25083 '새싹' [ 22/05/17 09:26:15 ] by Alub

---
+++ BOJ/25083/25083_1.c
@@ -0,0 +1,15 @@
+/**
+* 메모리: 1112 KB, 시간: 0 ms
+* 2022.05.17
+* by Alub
+*/
+#include<stdio.h>
+int main(void){
+    printf("         ,r\'\"7\n");
+    printf("r`-_   ,\'  ,/\n");
+    printf(" \\. \". L_r\'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |");
+    return 0;
+}
