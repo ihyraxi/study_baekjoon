2d3d7da1aa6986736e9deb2fe3ccd38935dd7b33
ihyraxi <100103351+ihyraxi@users.noreply.github.com>
2022-05-17 09:45:11 +0900

    BOJ-10869 '사칙연산' [ 22/05/17 09:45:11 ] by Alub

---
+++ BOJ/10869/10869_3.c
@@ -0,0 +1,16 @@
+/**
+* 메모리: 1112 KB, 시간: 0 ms
+* 2022.05.17
+* by Alub
+*/
+#include <stdio.h>
+int main(void) {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a+b);
+	printf("%d\n", a-b);
+	printf("%d\n", a*b);
+	printf("%d\n", a/b);
+	printf("%d\n", a%b);
+	return 0;
+}
