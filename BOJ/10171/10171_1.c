d24ff982db9b95048d193e5823f090aa059952b1
ihyraxi <100103351+ihyraxi@users.noreply.github.com>
2022-05-17 09:23:49 +0900

    BOJ-10171 '고양이' [ 22/05/17 09:23:49 ] by Alub

---
+++ BOJ/10171/10171_1.c
@@ -0,0 +1,13 @@
+/**
+* 메모리: 1112 KB, 시간: 0 ms
+* 2022.05.17
+* by Alub
+*/
+#include<stdio.h>
+int main(void){
+    printf("\\    /\\\n");
+    printf(" )  ( \')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|");
+    return 0;
+}
